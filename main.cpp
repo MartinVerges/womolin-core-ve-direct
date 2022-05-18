cb4a2607c8885c4223fbdab65ce768ed96819a3c
Martin Verges <martin.verges@croit.io>
2022-05-18 12:35:47 +0200

    moved to serial in blocking mode

--- main.cpp
+++ main.cpp
@@ -15,6 +15,23 @@ using namespace std;
 
 VeDirectFrameHandler veDirectFrameHandler;
 
+/**
+ * @brief Set the Socket Blocking mode
+ * 
+ * @param fd socket to modify
+ * @param blocking true for blocking, false for non-blocking
+ * @return true on success
+ * @return false  on error
+ */
+bool SetSocketBlockingEnabled(int fd, bool blocking)
+{
+   if (fd < 0) return false;
+   int flags = fcntl(fd, F_GETFL, 0);
+   if (flags == -1) return false;
+   flags = blocking ? (flags & ~O_NONBLOCK) : (flags | O_NONBLOCK);
+   return (fcntl(fd, F_SETFL, flags) == 0) ? true : false;
+}
+
 int main(int argc, char* argv[]) {
   string filename = "";
 
@@ -37,33 +54,24 @@ int main(int argc, char* argv[]) {
   // Configure serial port
   struct termios tty;
   memset(&tty, 0, sizeof tty);
-  if (tcgetattr(serialport, &tty) != 0 ) {
+  if (tcgetattr(serialport, &tty) != 0) {
     cout << "Error " << errno << " from tcgetattr: " << strerror(errno) << endl;
   }
   // Set Baud Rate
   cfsetospeed (&tty, B19200);
   cfsetispeed (&tty, B19200);
-  
   // Make raw
   cfmakeraw(&tty);
-
   // Flush Port, then applies attributes
   tcflush(serialport, TCIFLUSH);
-  if (tcsetattr(serialport, TCSANOW, &tty ) != 0) {
+  if (tcsetattr(serialport, TCSANOW, &tty) != 0) {
     cout << "Error " << errno << " from tcsetattr" << endl;
   }
+  SetSocketBlockingEnabled(serialport, true);
 
   char buf = '\0';
-  int nread = 0;
   while(true) {
-    nread = read(serialport, &buf, sizeof buf);
-    switch (nread) {
-      case -1:
-        break;
-      case 0: // nothing new
-        usleep(10);
-        break;
-      default:
+    if (read(serialport, &buf, sizeof buf) ) {
         veDirectFrameHandler.rxData(buf);
         if (veDirectFrameHandler.isDataAvailable()) {
           for (int i = 0; i < veDirectFrameHandler.veEnd; i++ ) {
@@ -72,7 +80,6 @@ int main(int argc, char* argv[]) {
           }
           veDirectFrameHandler.clearData();
         }
-        break;
     }
   }
 
